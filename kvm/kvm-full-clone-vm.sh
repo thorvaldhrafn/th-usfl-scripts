d4b0eae7af0bccbf99371149632396b069d90199
thorvald <thorvald@gridinsoft.com>
2021-11-16 18:00:55 +0200

    First

--- kvm/kvm-full-clone-vm.sh
+++ kvm/kvm-full-clone-vm.sh
@@ -17,13 +17,19 @@ virsh define $dst_vm_name.xml
 new_uuid=$(virsh domuuid $dst_vm_name)
 new_mac=$(virsh domiflist $dst_vm_name | sed -e '1,2d' -e '/^$/d' | awk '{ print $ 5}')
 
-virsh snapshot-list $src_vm_name --tree | sed -E "s/ *.{1,2} +//" | sed -E "s/\|//" | sed -E "/^$/d" |sed -e '1,2d' -e '/^$/d'|cut -d' ' -f2| while read -r line; do
+virsh snapshot-list $src_vm_name --tree | sed -E "s/ *.{1,2} +//" | sed -E "s/\|//" | sed -E "/^$/d" | while read -r line; do
+  echo $line
   new_snpsh_name=$(echo "${line}" | sed "s/${src_vm_name}/${dst_vm_name}/")
   virsh snapshot-dumpxml $src_vm_name --snapshotname "${line}" --security-info > "${line}.xml"
   sed -i -E "s/(.*<uuid>)${old_uuid}(<\/uuid>)/\1${new_uuid}\2/" "${line}.xml"
   sed -i -E "s/(.*<mac address=')${old_mac}('\/>)/\1${new_mac}\2/" "${line}.xml"
   sed -i -E "s/(.*<name>)${src_vm_name}(<\/name>)/\1${dst_vm_name}\2/" "${line}.xml"
   sed -i -E "s/(.*<name>)${line}(<\/name>)/\1${new_snpsh_name}\2/" "${line}.xml"
+  sed -i -E "s/(.*<name>)${line}(<\/name>)/\1${new_snpsh_name}\2/" "${line}.xml"
+  sed -i -E "s/(.*<name>)${parrent_snpsh_name}(<\/name>)/\1${prev_snpsh_name}\2/" "${line}.xml"
   sed -i -E "s/(.*<source file='\/var\/lib\/libvirt\/images\/)${src_vm_name}(\.qcow2'\/>)/\1${dst_vm_name}\2/" "${line}.xml"
+  parrent_snpsh_name=${line}
+  prev_snpsh_name=${new_snpsh_name}
   virsh snapshot-create $dst_vm_name "${line}.xml" --redefine
+  sleep 5
 done
