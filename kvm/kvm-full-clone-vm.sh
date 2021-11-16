84a9f83d4502a7e5cc0d53170332a4ff12947d64
thorvald <thorvald@gridinsoft.com>
2021-11-16 16:25:45 +0200

    First

--- kvm/kvm-full-clone-vm.sh
+++ kvm/kvm-full-clone-vm.sh
@@ -2,26 +2,28 @@
 
 #Full clone VM vm_source_name to vm_dest_name with all snapshots
 
-Source_VM_Name="vm_source_name"
-Destination_VM_Name="vm_dest_name"
-old_uuid=$(virsh domuuid $Source_VM_Name)
-old_mac=$(virsh domiflist $Source_VM_Name | sed -e '1,2d' -e '/^$/d' | awk '{ print $ 5}')
+src_vm_name="vm_source_name"
+dst_vm_name="vm_dest_name"
+old_uuid=$(virsh domuuid $src_vm_name)
+old_mac=$(virsh domiflist $src_vm_name | sed -e '1,2d' -e '/^$/d' | awk '{ print $ 5}')
 
-virt-clone --original $Source_VM_Name --name $Destination_VM_Name --file /var/lib/libvirt/images/$Destination_VM_Name.qcow2 --print-xml > $Destination_VM_Name.xml
+virt-clone --original $src_vm_name --name $dst_vm_name --file /var/lib/libvirt/images/$dst_vm_name.qcow2 --print-xml > $dst_vm_name.xml
 
-rsync -ah --progress /var/lib/libvirt/images/$Source_VM_Name.qcow2 /var/lib/libvirt/images/$Destination_VM_Name.qcow2
+rsync -ah --progress /var/lib/libvirt/images/$src_vm_name.qcow2 /var/lib/libvirt/images/$dst_vm_name.qcow2
 
-virt-sysprep -a /var/lib/libvirt/images/$Destination_VM_Name.qcow2
+virt-sysprep -a /var/lib/libvirt/images/$dst_vm_name.qcow2
 
-virsh define $Destination_VM_Name.xml
-new_uuid=$(virsh domuuid $Destination_VM_Name)
-new_mac=$(virsh domiflist $Destination_VM_Name | sed -e '1,2d' -e '/^$/d' | awk '{ print $ 5}')
+virsh define $dst_vm_name.xml
+new_uuid=$(virsh domuuid $dst_vm_name)
+new_mac=$(virsh domiflist $dst_vm_name | sed -e '1,2d' -e '/^$/d' | awk '{ print $ 5}')
 
-virsh snapshot-list $Source_VM_Name |sed -e '1,2d' -e '/^$/d'|cut -d' ' -f2| while read -r line; do
-  virsh snapshot-dumpxml $Source_VM_Name --snapshotname "${line}" --security-info > "${line}.xml"
+virsh snapshot-list $src_vm_name |sed -e '1,2d' -e '/^$/d'|cut -d' ' -f2| while read -r line; do
+  new_snpsh_name=$(echo "${line}" | sed "s/${src_vm_name}/${dst_vm_name}/")
+  virsh snapshot-dumpxml $src_vm_name --snapshotname "${line}" --security-info > "${line}.xml"
   sed -i -E "s/(.*<uuid>)${old_uuid}(<\/uuid>)/\1${new_uuid}\2/" "${line}.xml"
   sed -i -E "s/(.*<mac address=')${old_mac}('\/>)/\1${new_mac}\2/" "${line}.xml"
-  sed -i -E "s/(.*<name>)${Source_VM_Name}(<\/name>)/\1${Destination_VM_Name}\2/" "${line}.xml"
-  sed -i -E "s/(.*<source file='\/var\/lib\/libvirt\/images\/)${Source_VM_Name}(\.qcow2'\/>)/\1${Destination_VM_Name}\2/" "${line}.xml"
-  virsh snapshot-create $Destination_VM_Name "${line}.xml" --redefine
+  sed -i -E "s/(.*<name>)${src_vm_name}(<\/name>)/\1${dst_vm_name}\2/" "${line}.xml"
+  sed -i -E "s/(.*<name>)${line}(<\/name>)/\1${new_snpsh_name}\2/" "${line}.xml"
+  sed -i -E "s/(.*<source file='\/var\/lib\/libvirt\/images\/)${src_vm_name}(\.qcow2'\/>)/\1${dst_vm_name}\2/" "${line}.xml"
+  virsh snapshot-create $dst_vm_name "${line}.xml" --redefine
 done
